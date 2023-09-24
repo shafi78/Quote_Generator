98962cc2a65ecbf723e464ddbac733deec6bebca
shafi <shafiullaattar786@gmail.com>
2023-09-24 19:56:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+
+
+const apiURL = "https://api.quotable.io/random";
+
+async function getQuote(url) {
+    const response = await fetch(url);
+    let data = await response.json();
+    quote.innerHTML = data.content ;
+    author.innerHTML = data.author ;
+}
+
+getQuote(apiURL);
+
+function tweet() {
+    window.open("https://twitter.com/intent/tweet?text="+quote.innerHTML,"Tweet Window","width=600,height=300");
+}
